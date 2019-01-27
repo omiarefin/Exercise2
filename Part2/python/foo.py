afa912b07444c17f03228fc720b5ea2453550aa7
Arefin <omiarefin@github.com>
2019-01-27 17:47:52 +0100

    Python code done

--- Part2/python/foo.py
+++ Part2/python/foo.py
@@ -1,34 +1,40 @@
 # Python 3.3.3 and 2.7.6
 # python fo.py
-
+import threading
 from threading import Thread
-
 # Potentially useful thing:
 #   In Python you "import" a global variable, instead of "export"ing it when you declare it
 #   (This is probably an effort to make you feel bad about typing the word "global")
+lock = threading.Lock()
+
 i = 0
 
 def incrementingFunction():
     global i
+    with lock:
     # TODO: increment i 1_000_000 times
-
+     for j in range(0,1000000):
+        i+=1
 def decrementingFunction():
     global i
+    with lock:
     # TODO: decrement i 1_000_000 times
-
-
+     for j in range (0,1000000):
+        i-=1
 
 def main():
     global i
 
     incrementing = Thread(target = incrementingFunction, args = (),)
     decrementing = Thread(target = decrementingFunction, args = (),)
-    
+
     # TODO: Start both threads
-    
+    incrementing.start()
+    decrementing.start()
+
     incrementing.join()
     decrementing.join()
-    
+
     print("The magic number is %d" % (i))
 
 
