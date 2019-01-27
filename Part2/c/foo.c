74520ef4fb59a3b8d79852b5ec5c591341bebaba
Arefin <omiarefin@github.com>
2019-01-27 16:39:33 +0100

    C code done

--- Part2/c/foo.c
+++ Part2/c/foo.c
@@ -3,33 +3,44 @@
 
 int i = 0;
 
+pthread_mutex_t lock;
+
 // Note the return type: void*
 void* incrementingThreadFunction(){
+    pthread_mutex_lock(&lock);
     for (int j = 0; j < 1000000; j++) {
-	// TODO: sync access to i
-	i++;
+	  // TODO: sync access to i
+	     i++;
     }
+    pthread_mutex_unlock(&lock);
     return NULL;
 }
 
 void* decrementingThreadFunction(){
+  pthread_mutex_lock(&lock);
     for (int j = 0; j < 1000000; j++) {
-	// TODO: sync access to i
-	i--;
+  	// TODO: sync access to i
+	     i--;
     }
+    pthread_mutex_unlock(&lock);
     return NULL;
 }
 
 
 int main(){
-    pthread_t incrementingThread, decrementingThread;
-    
+    pthread_mutex_init(&lock,NULL);
+
+    pthread_t incrementingThread;
+    pthread_t decrementingThread;
+
     pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
     pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
-    
+
     pthread_join(incrementingThread, NULL);
     pthread_join(decrementingThread, NULL);
-    
+
+    pthread_mutex_destroy(&lock);
+
     printf("The magic number is: %d\n", i);
     return 0;
 }
